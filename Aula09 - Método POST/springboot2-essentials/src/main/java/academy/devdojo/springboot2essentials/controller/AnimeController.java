d11530a7e7a64b5e5ed6788b6db0eb327a28b3f0
unknown <jair.lopes@fatec.sp.gov.br>
2022-09-08 00:04:06 -0300

    Spring Boot 2 Essentials 09 - Método POST

--- Aula09 - Método POST/springboot2-essentials/src/main/java/academy/devdojo/springboot2essentials/controller/AnimeController.java
+++ Aula09 - Método POST/springboot2-essentials/src/main/java/academy/devdojo/springboot2essentials/controller/AnimeController.java
@@ -6,9 +6,11 @@ import academy.devdojo.springboot2essentials.util.DateUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -32,11 +34,6 @@ public class AnimeController {
     @GetMapping("/{id}")
     public HttpEntity<Anime> findById(@PathVariable Long id) {
         return ResponseEntity.ok(this.animeService.findById(id));
-    }
-
-    @PostMapping
-    public ResponseEntity<Anime> save(@RequestBody Anime anime) {
-        return new ResponseEntity(this.animeService.save(anime), HttpStatus.CREATED);
     }
 }
 
